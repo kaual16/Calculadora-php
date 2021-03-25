4de9b1a07fb279ac161dd23d29ccd0265da80f34
kaual16 <61718171+kaual16@users.noreply.github.com>
2021-03-25 15:44:44 -0300

    Add files via upload

--- calculadora.php
+++ calculadora.php
@@ -11,9 +11,11 @@ $tipo = $_POST['tipo'];
         case 'Somar' : $resultado = $Valor1 + $Valor2; break;
         case 'Subtrair' : $resultado = $Valor1 - $Valor2; break;
         case 'Multiplicação' : $resultado = $Valor1 * $Valor2; break;
-        case 'Divisão' : $resultado = $Valor1 / $Valor2; break;
+        case 'Divisão' : $resultado = ($Valor2 != 0) ? $Valor1 / $Valor2 : '0 não é divisivel'; break;
+        
     }
 
+ 
     echo $resultado;
 
 
